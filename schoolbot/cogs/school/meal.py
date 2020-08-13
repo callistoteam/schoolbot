f9a5a2a0fda78203c719d8dca9f9926ba72bdfc8
SaidBySolo <saidbysolo@gmail.com>
2020-08-13 16:03:33 +0900

    feat: choice school, choice date

--- schoolbot/cogs/school/meal.py
+++ schoolbot/cogs/school/meal.py
@@ -5,42 +5,51 @@ import neispy
 import discord
 from discord.ext import commands
 
-neis = neispy.AsyncClient(os.environ["API_KEY"])
-
 
 class Meal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="급식")
-    async def _meal(self, ctx, school_name: str):
+    async def _meal(self, ctx, school_name: str, date: int = None):
+        msg = await ctx.send("정보를 요청합니다 잠시만 기다려주세요.")
+        neis = neispy.AsyncClient(os.environ["API_KEY"])
         scinfo = await neis.schoolInfo(SCHUL_NM=school_name, rawdata=True)
         if len(scinfo.data) > 1:
             school_name_list = [school_name["SCHUL_NM"] for school_name in scinfo.data]
-            school_name_list_with_num = [str(index) + ". " + school_names for index, school_names in enumerate(school_name_list, 1)]
-            await ctx.send("\n".join(school_name_list_with_num))
-            def check2(m):
-                return m.user == ctx.author
+            school_name_list_with_num = [
+                str(index) + ". " + school_names
+                for index, school_names in enumerate(school_name_list, 1)
+            ]
+            await msg.edit("\n".join(school_name_list_with_num))
+
+            def check(m):
+                return m.author == ctx.author
+
             try:
-                respon = await self.bot.wait_for('message', check=check2, timeout=30)
+                respon = await self.bot.wait_for("message", check=check, timeout=30)
             except asyncio.TimeoutError:
-                await ctx.send("시간 초과입니다.")
+                await msg.edit("시간 초과입니다.")
             else:
                 fetch_msg = await ctx.fetch_message(respon.id)
                 try:
-                    num = int(fetmsg.content) - 1
+                    num = int(fetch_msg.content) - 1
                 except ValueError:
                     return await ctx.send("잘못된 값을 주셧습니다. 처음부터 다시 시도해주세요")
                 else:
-                    choice = scinfo[num]
+                    choice = scinfo.data[num]
                     AE = choice["ATPT_OFCDC_SC_CODE"]
                     SE = choice["SD_SCHUL_CODE"]
         else:
-            AE = scinfo.ATPT_OFCDC_SC_CODE
-            SE = scinfo.SD_SCHUL_CODE
+            choice = scinfo.data[0]
+            AE = choice["ATPT_OFCDC_SC_CODE"]
+            SE = choice["SD_SCHUL_CODE"]
 
-        scmeal = await neis.mealServiceDietInfo(AE,SE)
-        meal = scmeal.DDISH_NM.replace('<br/>', '\n')
-        await ctx.send(meal)
-            
-            
+        if date is None:
+            scmeal = await neis.mealServiceDietInfo(AE, SE)
+        else:
+            scmeal = await neis.mealServiceDietInfo(AE, SE, MLSV_YMD=date)
+
+        meal = scmeal.DDISH_NM.replace("<br/>", "\n")
+        await msg.edit(meal)
+
