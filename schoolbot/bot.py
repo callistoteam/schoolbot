a56f1e0f1afa09d77e83928f441e5401ec37520c
SaidBySolo <saidbysolo@gmail.com>
2020-08-13 10:02:27 +0900

    feat: file structure design

---
+++ schoolbot/bot.py
@@ -0,0 +1,20 @@
+import os
+
+token = os.environ["TOKEN"]
+
+
+def load_cogs(bot):
+    extensions = ["cogs.events"]
+    failed_list = []
+
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f"{e.__class__.__name__}: {e}")
+            failed_list.append(extension)
+
+    if failed_list:
+        print(f"\nFailed to load extension. \n{''.join(failed_list)}")
+
+    return failed_list
