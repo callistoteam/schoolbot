5b71d27d2ff76a2e3b4b2d2b632d36292c722e1d
SaidBySolo <saidbysolo@gmail.com>
2020-08-13 10:38:21 +0900

    style: change prefix

--- schoolbot/__main__.py
+++ schoolbot/__main__.py
@@ -1,6 +1,6 @@
 from .bot import load_cogs
 from discord.ext.commands import Bot as Core
 
-bot = Core(command_prefix="?", help_command=None)
+bot = Core(command_prefix="*", help_command=None)
 load_cogs(bot)
 bot.run(token)
