16bac94f1004f7e81f8bd3a4a7beac8681d674ab
Ryu JuHeon <saidbysolo@gmail.com>
2020-08-18 17:44:29 +0900

    fix: apikey error

--- schoolbot/cogs/school/__init__.py
+++ schoolbot/cogs/school/__init__.py
@@ -2,12 +2,12 @@ import os
 
 from .meal import Meal
 from .academic_schedule import AcademicSchedule
-from .article import Article
 from .timetable import TimeTable
+from .article import Article
 
 
 def setup(bot):
     bot.add_cog(Meal(bot, os.environ["API_KEY"]))
     bot.add_cog(AcademicSchedule(bot, os.environ["API_KEY"]))
+    bot.add_cog(TimeTable(bot, os.environ["API_KEY"]))
     bot.add_cog(Article(bot))
-    bot.add_cog(TimeTable(bot))
