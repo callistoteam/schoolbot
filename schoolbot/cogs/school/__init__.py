b37f90b4f8a30d5883f14df62825d9cfb7c7f639
Ryu JuHeon <saidbysolo@gmail.com>
2020-08-18 17:41:26 +0900

    feat: add timetable command

--- schoolbot/cogs/school/__init__.py
+++ schoolbot/cogs/school/__init__.py
@@ -3,9 +3,11 @@ import os
 from .meal import Meal
 from .academic_schedule import AcademicSchedule
 from .article import Article
+from .timetable import TimeTable
 
 
 def setup(bot):
     bot.add_cog(Meal(bot, os.environ["API_KEY"]))
     bot.add_cog(AcademicSchedule(bot, os.environ["API_KEY"]))
     bot.add_cog(Article(bot))
+    bot.add_cog(TimeTable(bot))
