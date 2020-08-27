88f41f44633066c8cc355be9c20cbf2607c6f5f0
SaidBySolo <saidbysolo@gmail.com>
2020-08-28 08:14:06 +0900

    refactor: resolve conversation

--- schoolbot/cogs/school/timetable.py
+++ schoolbot/cogs/school/timetable.py
@@ -23,11 +23,6 @@ class TimeTable(commands.Cog):
         class_nm: int = None,
         date: int = None,
     ):
-        msg = await ctx.send(
-            embed=discord.Embed(
-                title="정보를 요청합니다 잠시만 기다려주세요.", colour=discord.Colour.blurple()
-            )
-        )
         if school_name and school_name.isdigit():
             if len(school_name) == 8:
                 date = int(school_name)
@@ -46,6 +41,11 @@ class TimeTable(commands.Cog):
             )
         user_data = await db.get_user_data(ctx.author.id)
         if user_data and school_name == None:
+            msg = await ctx.send(
+                embed=discord.Embed(
+                    title="정보를 요청합니다 잠시만 기다려주세요.", colour=discord.Colour.blurple()
+                )
+            )
             AE = user_data[1]
             SE = user_data[2]
             if not grade:
@@ -95,6 +95,11 @@ class TimeTable(commands.Cog):
 
         else:
             if school_name:
+                msg = await ctx.send(
+                    embed=discord.Embed(
+                        title="정보를 요청합니다 잠시만 기다려주세요.", colour=discord.Colour.blurple()
+                    )
+                )
                 try:
                     scinfo = await self.neis.schoolInfo(
                         SCHUL_NM=school_name, rawdata=True
@@ -160,7 +165,7 @@ class TimeTable(commands.Cog):
                 # AE = 대충 교육청코드
                 # SE = 대충 표준학교코드
                 # scclass = 대충 초 중 고 고르는거
-                return await msg.edit(
+                return await ctx.send(
                     embed=discord.Embed(
                         title="학교명을 입력해주세요", colour=discord.Colour.blurple()
                     )
