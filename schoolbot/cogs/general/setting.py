d4bbdaee43249ce0507d8cd81e0003ed4e2fe55d
SaidBySolo <saidbysolo@gmail.com>
2020-08-28 08:22:57 +0900

    refactor: resolve conversation

--- schoolbot/cogs/general/setting.py
+++ schoolbot/cogs/general/setting.py
@@ -11,7 +11,7 @@ class Setting(commands.Cog):
     @commands.command(name="설정")
     async def _setting(self, ctx, key: str = None, *, value: str = None):
         if key and value:
-            args = " ".join(value.split("|")).split()
+            args = value.split("|")
             if key == "학교":
                 if args[3].isdigit() and args[4].isdigit():
                     grade = int(args[3])
