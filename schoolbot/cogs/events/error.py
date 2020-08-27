940de7f053d77aa8035ad20c25799619f646b04a
SaidBySolo <saidbysolo@gmail.com>
2020-08-28 07:56:24 +0900

    fix: question mark is the most used, so it can be inconvenient.

--- schoolbot/cogs/events/error.py
+++ schoolbot/cogs/events/error.py
@@ -16,10 +16,7 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            await ctx.send(
-                "명령어를 찾을 수 없습니다. `?도움말` 명령어를 사용해 전체 명령어 목록을 볼 수 있습니다.", delete_after=5
-            )
-
+            pass
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(
                 "명령어 사용법이 잘못되었습니다. 값이 부족합니다. `?도움말` 명령어를 통해 정확한 사용법을 보실 수 있습니다.",
