c99af1190e25bbf070ec9f477b772a9b4b2dffaf
kijk2869 <mareowst@gmail.com>
2020-09-05 23:54:04 +0900

    feat: meal command
    Co-Authored-By: Ryu JuHeon <saidbysolo@gmail.com>

--- bot.py
+++ bot.py
@@ -1,17 +1,81 @@
+import asyncio
 import os
-import cogs
+
+import neispy
+from neispy import DataNotFound
+
+import discord
 from discord.ext import commands
+
+import cogs
 import database
+
+os.environ["MEAL_API_KEY"] = "youshallnotpass"
 
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.neis = neispy.Client()  # os.environ["API_KEY"])
+
         cogs.load(self)
 
         self.loop.create_task(database.init())
 
+    async def search_school(self, ctx, query: str) -> dict:
+        try:
+            Data = await self.neis.schoolInfo(SCHUL_NM=query)
+        except neispy.DataNotFound:
+            await ctx.send(
+                embed=discord.Embed(
+                    title="학교 정보가 없습니다. 확인 후 다시 시도해주세요.", colur=discord.Colour.red()
+                )
+            )
+            return
+
+        if len(Data) == 1:
+            return Data[0]
+
+        school_list = [
+            f"{index}. {school.SCHUL_NM} ({school.LCTN_SC_NM})"
+            for index, school in enumerate(Data, 1)
+        ]
+
+        message = await ctx.send(
+            embed=discord.Embed(
+                title="여러개의 검색 결과입니다.",
+                description="".join(school_list),
+                colur=discord.Colour.blurple(),
+            )
+        )
+
+        try:
+            response = await self.wait_for(
+                "message",
+                check=lambda m: m.author == ctx.author and m.channel == message.channel,
+                timeout=30.0,
+            )
+        except asyncio.TimeoutError:
+            await message.edit(
+                embed=discord.Embed(
+                    title="시간 초과 입니다. 처음부터 다시 시도 해주세요.", colur=discord.Colour.red()
+                )
+            )
+            return
+
+        if not response.content.isdigit():
+            await message.edit(
+                embed=discord.Embed(
+                    title="잘못된 값을 받았습니다. 확인 후 다시 시도 해주세요.", colur=discord.Colour.red()
+                )
+            )
+            return
+
+        return Data[int(response.content) - 1]
+
 
 if __name__ == "__main__":
-    Bot(command_prefix="?", help_command=None).run(os.environ["TOKEN"])
+    Bot(command_prefix="?", help_command=None).run(
+        "NTAyNDczMzI1OTY1MjEzNzE4.W8iKwA.Vm0KP8BSGqyascqHjCANG842gCA"
+    )  # os.environ["TOKEN"])
---
+++ cogs/meal.py
@@ -0,0 +1,73 @@
+import os
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+import aiohttp
+import urllib.parse
+import neispy
+from database import User
+
+
+class Meal(commands.Cog):
+    def __init__(self, Bot):
+        self.Bot = Bot
+
+    async def __get_meal_image(self, meals):
+        async with aiohttp.ClientSession() as session:
+            meals = "&meal=".join([urllib.parse.quote(x) for x in meals.split("<br/>")])
+            async with session.post(
+                f"https://api.schoolbot.callisto.team/render/?meal={meals}",
+                headers={"Authorization": os.environ["MEAL_API_KEY"]},
+            ) as response:
+                return (await response.json())["url"]
+
+    @commands.command(name="급식")
+    async def _meal(self, ctx, schoolname: str = None, date: str = neispy.now()):
+        if schoolname and schoolname.isdigit():
+            schoolname, date = None, schoolname
+
+        if schoolname:
+            School = await self.Bot.search_school(ctx, schoolname)
+            if not School:
+                return
+
+            AE, SE = School.ATPT_OFCDC_SC_CODE, School.SD_SCHUL_CODE
+        else:
+            Data = await User.get_or_none(id=ctx.author.id)
+            if not Data:
+                return await ctx.send(
+                    embed=discord.Embed(
+                        title="학교명을 입력 해주시기 바랍니다.", colur=discord.Colour.red()
+                    )
+                )
+
+            AE, SE = Data.neis_ae, Data.neis_se
+
+        try:
+            meal = await self.Bot.neis.mealServiceDietInfo(AE, SE, MLSV_YMD=date)
+        except neispy.DataNotFound:
+            return await ctx.send(
+                embed=discord.Embed(
+                    title="해당하는 날짜에 급식이 없는거 같습니다.", colur=discord.Colour.red()
+                )
+            )
+        else:
+            meal = meal[0]
+
+        embed = discord.Embed(
+            title=f"{meal.SCHUL_NM}의 급식입니다.", colour=0x2E3136
+        ).add_field(
+            name=datetime.strptime(meal.MLSV_YMD, "%Y%m%d").strftime("%Y년 %m월 %d일"),
+            value=meal.DDISH_NM.replace("<br/>", "\n"),
+        )
+
+        message = await ctx.send(embed=embed)
+
+        embed.set_image(url=await self.__get_meal_image(meal.DDISH_NM))
+
+        await message.edit(embed=embed)
+
+
+def setup(Bot):
+    Bot.add_cog(Meal(Bot))
