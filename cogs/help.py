acedbe32b66d1e82c9cde14f9a01e1928a580e26
Ryu JuHeon <saidbysolo@gmail.com>
2020-09-06 12:27:11 +0900

    fix: position

--- cogs/help.py
+++ cogs/help.py
@@ -9,18 +9,18 @@ class Help(commands.Cog):
     @commands.command(name="도움말", aliases=["help", "도움", "commands", "명령어"])
     async def _help(self, ctx):
         embed = discord.Embed(title="도움말", description="접두사: ``?``")
-        embed.add_field(
-            name="유용한 링크",
-            value="[이용약관](https://callisto.team/tos)\n[개인정보취급방침](https://callisto.team/privacy)",
-            inline=False,
-        )
+
         for Command in [i for i in self.Bot.commands if i.help]:
             embed.add_field(
                 name=Command.name,
                 value=Command.help,
                 inline=False,
             )
-
+        embed.add_field(
+            name="유용한 링크",
+            value="[이용약관](https://callisto.team/tos)\n[개인정보취급방침](https://callisto.team/privacy)",
+            inline=False,
+        )
         await ctx.send(embed=embed)
 
 
