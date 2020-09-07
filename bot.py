be8da03690cc04d886beff42327c760a19d14640
kijk2869 <mareowst@gmail.com>
2020-09-07 10:20:33 +0900

    fix: version up

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Bot(commands.Bot):
-    __version__ = "1.1.1"
+    __version__ = "1.1.2"
 
     def __init__(self, *args, **kwargs):
         self.initialize_autoembed()
