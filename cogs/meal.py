5b84c9d1b00d3319cbcaefc8d7c522f1a37a05d9
Ryu JuHeon <saidbysolo@gmail.com>
2020-10-09 16:35:30 +0900

    refactor(cogs.meal): apply rewrite mealapi

--- cogs/meal.py
+++ cogs/meal.py
@@ -1,6 +1,7 @@
 import os
 import urllib.parse
 from datetime import datetime
+import re
 
 import aiohttp
 import discord
@@ -16,11 +17,16 @@ class Meal(commands.Cog):
         self.Bot = Bot
 
     async def __get_meal_image(self, meals):
+        regex = re.compile(r"(\d+)\.")
+        meal_list = [
+            {"meal": f"{regex.sub('', info)}", "allergy": f"{regex.findall(info)}"}
+            for info in meals.split("<br/>")
+        ]
         async with aiohttp.ClientSession() as session:
-            meals = "&meal=".join([urllib.parse.quote(x) for x in meals.split("<br/>")])
             async with session.post(
-                f"https://api.schoolbot.callisto.team/render/?meal={meals}",
+                f"https://api.schoolbot.callisto.team/render",
                 headers={"Authorization": os.environ["MEAL_API_KEY"]},
+                json={"meal": meal_list},
             ) as res:
                 return (await res.json())["url"]
 
