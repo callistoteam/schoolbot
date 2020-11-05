c18ae6691dccfcd1f13027aca7fccfb1c3e3c67c
원더 <wonderbotsupport@naver.com>
2020-11-05 22:01:48 +0900

    feat: added ez key

--- utils/format_date.py
+++ utils/format_date.py
@@ -7,6 +7,15 @@ def format_date(argument):
     current_date = datetime.datetime.now(tz=KST)
     if not argument:
         date = ( current_date.year, current_date.month, current_date.day )
+    elif argument == '내일':
+        tomorrow = current_date + datetime.timedelta(days=1)
+        date = ( tomorrow.year, tomorrow.month, tomorrow.day )
+    elif argument == '어제':
+        yesterday = current_date - datetime.timedelta(days=1)
+        date = ( yesterday.year, yesterday.month, yesterday.day )
+    elif argument == '모레':
+        day_after_tomorrow = current_date + datetime.timedelta(days=2)
+        date = ( day_after_tomorrow.year, day_after_tomorrow.month, day_after_tomorrow.day )
     else: 
         date = re.match(r'^(?:(?P<YY>\d{4}|\d{2})[| ])?(?P<MM>\d{2})[| ](?P<DD>\d{2})|$', argument) or re.match(r'^(?:(?P<YY>\d{4}|\d{2})년[| ])?(?P<MM>\d{1,2})월[| ](?P<DD>\d{1,2})일$', argument)
     
